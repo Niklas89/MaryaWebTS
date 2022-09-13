bc9980ed99911474abac194ec75d98ba588c9c81
Niklas <niklas_edelstam@hotmail.com>
2022-09-13 11:18:08 +0200

    login tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import Navbar from './components/Navbar';
 
@@ -9,7 +10,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    < Navbar />
     <App />
   </React.StrictMode>
 );
