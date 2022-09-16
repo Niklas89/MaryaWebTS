2f3abddc6ede7b39721dd884b42d17b3ec367c32
Nanaplouf <vera78ds@gmail.com>
2022-09-16 11:37:00 +0200

    Mise a jour des any et des simple cotes

--- src/components/form/FormRegister.tsx
+++ src/components/form/FormRegister.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import { IUser } from '../../interfaces/IUser';
+import { IUser } from "../../interfaces/IUser";
 import * as Yup from "yup";
 import { FormFieldType, useFormBuilder } from "./FormModel";
 import { FormikValues } from "formik";
@@ -7,15 +7,15 @@ import { toast } from "react-toastify";
 import { Grid, TextField, Typography } from "@mui/material";
 import { AxiosFunction } from "../../api/AxiosFunction";
 import { useNavigate } from "react-router-dom";
-import { error } from "console";
+import { AxiosError, AxiosResponse } from "axios";
 
 
 const userFormFields: FormFieldType[] = [
-    { name: 'lastName', field: TextField, label: "Nom", isMultiLine: false },
-    { name: 'firstName', field: TextField, label: "Prenom", isMultiLine: false },
-    { name: 'email', field: TextField, label: "E-mail", isMultiLine: false },
-    { name: 'password', field: TextField, label: "Mot de passe", type: "password", isMultiLine: false },
-]
+    { name: "lastName", field: TextField, label: "Nom", isMultiLine: false },
+    { name: "firstName", field: TextField, label: "Prenom", isMultiLine: false },
+    { name: "email", field: TextField, label: "E-mail", isMultiLine: false },
+    { name: "password", field: TextField, label: "Mot de passe", type: "password", isMultiLine: false },
+];
 
 const FormRegister = () => {
 
@@ -29,33 +29,33 @@ const FormRegister = () => {
         password: "",
     };
 
-    const [userInfo, setUserInfos] = useState<IUser>(initialValues)
+    const [userInfo, setUserInfos] = useState<IUser>(initialValues);
 
-    const { postQuery } = AxiosFunction()
+    const { postQuery } = AxiosFunction();
 
     const validationShema = Yup.object().shape({
-        email: Yup.string().email("Votre e-mail n'est pas valide").required("Merci de remplir le champ e-mail"),
+        email: Yup.string().email("Votre e-mail n\'est pas valide").required("Merci de remplir le champ e-mail"),
         lastName: Yup.string().matches(/^[A-Za-z ]+$/, "Le nom doit contenir que des lettres.").required("Merci de remplir le champ nom"),
         firstName: Yup.string().matches(/^[A-Za-z ]+$/, "Le nom doit contenir que des lettres.").required("Merci de remplir le champ prenom"),
         password: Yup.string().min(6, "Mot de passe trop court").max(50, "Mot de passe trop long").matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d@$!%*#?&\/]{6,50}$/, "Le mot de passe doit contenir une majuscule, une minuscule, et un nombre.").required("Merci de remplir le champ mot de passe"),
-    })
+    });
 
     const handleSubmit = useCallback((values: FormikValues, callback: any) => {
 
-        const postData = { ...values, 'idRole': '1' };
+        const postData = { ...values, "idRole": "1" };
 
-        postQuery('user/register', postData).then((response) => {
+        postQuery("user/register", postData).then((response: AxiosResponse) => {
             setUserInfos(response?.data)
             navigate(from, { replace: true });
-        }).catch(() => {
-            toast.error('Ce compte existe déjà, merci de vous connecter.', {
+        }).catch((error: AxiosError) => {
+            toast.error("Ce compte existe déjà, merci de vous connecter.", {
                 position: "bottom-right",
                 autoClose: 3000,
                 hideProgressBar: true,
                 closeOnClick: true,
                 pauseOnHover: true,
                 draggable: true,
-                toastId: 'submit-dog-file-error'
+                toastId: "submit-dog-file-error"
 
             });
             return callback();
@@ -65,7 +65,7 @@ const FormRegister = () => {
 
     const { renderForm } = useFormBuilder(validationShema, userInfo, userFormFields,
         { submit: handleSubmit }
-    )
+    );
 
     return (
         <>
