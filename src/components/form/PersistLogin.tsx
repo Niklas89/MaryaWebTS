0c7b515cf5b6b5ea3f53de9aee740089327dcf51
Niklas <niklas_edelstam@hotmail.com>
2022-09-19 09:13:12 +0200

    add useRef for PersistLogin

--- src/components/form/PersistLogin.tsx
+++ src/components/form/PersistLogin.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import useRefreshToken from '../../hooks/useRefreshToken';
 import useAuth from '../../hooks/useAuth';
 
@@ -7,8 +7,11 @@ const PersistLogin = () => {
     const [isLoading, setIsLoading] = useState(true);
     const refresh = useRefreshToken();
     const { auth, persist } = useAuth();
+    const effectRan = useRef(false)
 
     useEffect(() => {
+
+        if (effectRan.current === false) {
         let isMounted = true; 
 
         // we want to run this only when authState is empty (after refresh the page and the state has been emptied)
@@ -31,7 +34,11 @@ const PersistLogin = () => {
         // otherwise we setIsLoading to false
         !auth?.accessToken && persist ? verifyRefreshToken() : setIsLoading(false);
 
-        return () =>  { isMounted = false; }
+        return () =>  { 
+            isMounted = false;
+            effectRan.current = true;
+         }
+        }
     }, [])
 
     useEffect(() => {
