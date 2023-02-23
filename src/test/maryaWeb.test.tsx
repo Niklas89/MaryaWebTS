ae9d474cd1f61775da8951e24ece183547c7dc49
ClementG60 <cle_guillaume@hotmail.fr>
2023-02-23 12:18:06 +0100

    modif test unitaire front

--- src/test/maryaWeb.test.tsx
+++ src/test/maryaWeb.test.tsx
@@ -1,17 +1,71 @@
 import axios from "../api/axios";
+import "@testing-library/jest-dom/extend-expect";
+import { ICategory } from "../interfaces/ICategory";
 
-describe("Get Caterogy and display them", () => {
-  // it("should return a code status 200", async () => {
-  //   const GETCATEGORY_URL = "/service/category";
-  //   expect(
-  //     await axios({
-  //       method: "get",
-  //       url: GETCATEGORY_URL,
-  //     }).then((res):any => res.status)
-  //   ).toBe(200);
+//test unitaire sur les catégories
+describe("Get Caterogy from API", () => {
+  const GETCATEGORY_URL = "/service/category";
+  var statusCode: number;
+  var dataResponse: ICategory;
 
-    // it("shoud return componants with title", () => {
-      
-    // })
-  //});
+  //test sur le code statut de la requête
+  it("should return a code status 200", async () => {
+    await axios({
+      method: "get",
+      url: GETCATEGORY_URL,
+    }).then((res) => {
+      statusCode = res.status;
+      dataResponse = res.data;
+    });
+    expect(statusCode).toBe(200);
+  });
+
+  //test sur le corps de la requête
+  it("should return an array with six categories", async () => {
+    await axios({
+      method: "get",
+      url: GETCATEGORY_URL,
+    }).then((res) => {
+      statusCode = res.status;
+      dataResponse = res.data;
+    });
+    expect(dataResponse).toStrictEqual([
+      {
+        id: 1,
+        name: "Coiffure",
+        createdAt: "2022-09-15T07:47:21.000Z",
+        updatedAt: "2022-09-15T07:47:21.000Z",
+      },
+      {
+        id: 2,
+        name: "Bricolage",
+        createdAt: "2022-09-15T07:47:21.000Z",
+        updatedAt: "2022-09-15T07:47:21.000Z",
+      },
+      {
+        id: 3,
+        name: "Jardinage",
+        createdAt: "2022-09-15T07:47:21.000Z",
+        updatedAt: "2022-09-15T07:47:21.000Z",
+      },
+      {
+        id: 4,
+        name: "Ménage",
+        createdAt: "2022-09-15T07:47:21.000Z",
+        updatedAt: "2022-09-15T07:47:21.000Z",
+      },
+      {
+        id: 5,
+        name: "Beauté",
+        createdAt: "2022-09-15T07:47:21.000Z",
+        updatedAt: "2022-09-15T07:47:21.000Z",
+      },
+      {
+        id: 6,
+        name: "Babysitting",
+        createdAt: "2022-09-15T07:47:21.000Z",
+        updatedAt: "2022-09-15T07:47:21.000Z",
+      },
+    ]);
+  });
 });
